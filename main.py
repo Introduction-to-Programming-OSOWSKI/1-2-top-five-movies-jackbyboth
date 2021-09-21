7d02699f71da2e1be2aca0faf91e3a1c0537fe9c
jackbyboth23 <817581@mystma.org>
2021-09-21 14:44:49 -0500

    2

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 def topFiveMovies(a, b, c, d, e,):
     return(a, b, c, d, e,)
-print(topFiveMovies("i want to eat your pancreas", "A Silent Voice", "Joker", "Your Name", "Weathering with You"))
+print (topFiveMovies("i want to eat your pancreas", "A Silent Voice", "Joker", "Your Name", "Weathering with You"))
